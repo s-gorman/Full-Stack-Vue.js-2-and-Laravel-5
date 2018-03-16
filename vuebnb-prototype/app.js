4fef141c99ac062f1cf16e1d425ea0a7152b43e0
S Gorman <gorman.17@gmail.com>
2018-03-16 19:27:07 -0400

    starting top of pg 36

---
+++ vuebnb-prototype/app.js
@@ -0,0 +1,8 @@
+var app = new Vue ({
+    el: '#app',
+    data: {
+        title: 'My apartment',
+        address: '12 My Street, My City, My Country',
+        about: 'This is a description of my apartment.'
+    }
+});
